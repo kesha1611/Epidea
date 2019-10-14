0ecefef66b147ea67cfdc300741d4e66b6087bcc
Kesha_Shah <keshahshah@gmail.com>
2019-10-14 12:34:11 +0530

    app complete

--- app/src/main/java/com/abuturab/FirebaseRecyclerviewWebview/ItemActivity.java
+++ app/src/main/java/com/abuturab/FirebaseRecyclerviewWebview/ItemActivity.java
@@ -1,6 +1,7 @@
 package com.abuturab.FirebaseRecyclerviewWebview;
 
 import android.content.Intent;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
